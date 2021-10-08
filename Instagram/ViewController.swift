1e350a6fae1dd15f1c202532bd817a3a524f0253
Nanae <42784268+nanaenolab@users.noreply.github.com>
2021-10-08 03:35:16 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by nanae itoi on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
